8bc7e83e862c8b50ee813226e058e708620a390a
Артем <zveronline@yandex.ru>
2022-06-24 12:34:52 +0300

    vue2 manifest

--- vue/isn-app/src/main.js
+++ vue/isn-app/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
